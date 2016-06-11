48b4f2ecfe00de336cdd6e84461eb117b980063d
francoiskha <francois.kha@gmail.com>
2016-06-11 17:25:58 +0200

    some things must not be run on linux

--- osx/homebrew-caveats.zsh
+++ osx/homebrew-caveats.zsh
@@ -1,11 +1,14 @@
-# using homebrew version of openssh, this a caveat
-eval $(ssh-agent) >/dev/null
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+	# using homebrew version of openssh, this a caveat
+	eval $(ssh-agent) >/dev/null
 
-function cleanup {
-	kill -9 $SSH_AGENT_PID
-}
-trap cleanup EXIT
+	function cleanup {
+		kill -9 $SSH_AGENT_PID
+	}
+	trap cleanup EXIT
 
-export GROOVY_HOME=/usr/local/opt/groovy/libexec
+	export GROOVY_HOME=/usr/local/opt/groovy/libexec
 
-[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+	[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh
+fi
