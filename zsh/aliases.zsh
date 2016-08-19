e0e5a99b0da9e442e6bcf0383e3a6355e4c16f32
francoiskha <francois.kha@gmail.com>
2016-08-19 23:30:24 +0200

    adding alias for file encrypting

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias publicip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias publicdn='dig +short myip.opendns.com @resolver1.opendns.com | xargs dig +short -x'
 alias sp="source $DOTFILES/zsh/prompt.zsh"
 alias agnoster="source $ZSH/themes/agnoster.zsh-theme"
-
 alias docker-machine="docker-machine --native-ssh" # waiting a fix for https://github.com/docker/machine/issues/1591
+alias encrypt="gpg -r 44772227 --encrypt-files"
 
 translate(){ wget -qO- "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=$1&langpair=$2|${3:-en}" | sed 's/.*"translatedText":"\([^"]*\)".*}/\1\n/'; }
