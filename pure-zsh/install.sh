76b055302049dc0c17c39fe37dde9e3f5a3a9fa3
kha <francois.kha@cbp-group.com>
2017-12-07 18:26:01 +0100

    station de travail au boulot

--- pure-zsh/install.sh
+++ pure-zsh/install.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
-
-git clone --depth 1 git@github.com:sindresorhus/pure.git $HOME/.pure-zsh
-ln -s $HOME/.pure-zsh/async.zsh $DOTFILES/pure-zsh/async
-ln -s $HOME/.pure-zsh/pure.zsh $DOTFILES/pure-zsh/prompt_pure_setup 
+#!/bin/bash
+if [[ ! "$PWD/pure-zsh/prompt_pure_setup " ]]
+then
+	echo "Installing pure-zsh for you."
+	git clone --depth 1 git@github.com:sindresorhus/pure.git $HOME/.pure-zsh
+	ln -s $HOME/.pure-zsh/async.zsh $PWD/pure-zsh/async
+	ln -s $HOME/.pure-zsh/pure.zsh $PWD/pure-zsh/prompt_pure_setup 
+fi
--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,6 @@
-easy_install SpoofMAC
+#!/bin/bash
+
+if [[ "$(uname -s)" == "Darwin" ]];
+then
+	easy_install SpoofMAC
+fi
