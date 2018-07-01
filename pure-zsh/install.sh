a3b3da26ae1268fff932b056a43d56fe6b99e228
François Kha <francoiskha@users.noreply.github.com>
2018-07-01 17:51:01 +0200

    fixing pure-zsh install

--- pure-zsh/install.sh
+++ pure-zsh/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [[ ! "$PWD/pure-zsh/prompt_pure_setup " ]]
+if [[ ! -f "$PWD/pure-zsh/prompt_pure_setup " ]]
 then
 	echo "Installing pure-zsh for you."
-	git clone --depth 1 git@github.com:sindresorhus/pure.git $HOME/.pure-zsh
+	git clone --depth 1 https://github.com/sindresorhus/pure.git $HOME/.pure-zsh
+	dos2unix $HOME/.pure-zsh/async.zsh
+	dos2unix $HOME/.pure-zsh/pure.zsh
 	ln -s $HOME/.pure-zsh/async.zsh $PWD/pure-zsh/async
 	ln -s $HOME/.pure-zsh/pure.zsh $PWD/pure-zsh/prompt_pure_setup 
 fi
