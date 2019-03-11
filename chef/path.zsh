e956c9566db743819c197dda874349ed6467f758
francoiskha <francois.kha@gmail.com>
2019-03-11 23:26:53 +0100

    do nothing if there is nothing

--- chef/path.zsh
+++ chef/path.zsh
@@ -1,3 +1,7 @@
-export PATH=/opt/chefdk/bin:$PATH
-export PATH=/opt/chefdk/embedded/bin:$PATH
-export PATH=${HOME}/.chefdk/gem/ruby/2.3.0/bin:$PATH
+if [ -d /opt/chefdk1/ ]; then
+	export PATH=/opt/chefdk/bin:$PATH
+	export PATH=/opt/chefdk/embedded/bin:$PATH
+fi
+if [ -d ${HOME}/.chefdk/gem/ruby/2.3.0 ]; then
+	export PATH=${HOME}/.chefdk/gem/ruby/2.3.0/bin:$PATH
+fi
