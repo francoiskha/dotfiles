92c48d47d9d7d824462e212f385746fa9ef913bc
francoiskha <francois.kha@gmail.com>
2020-01-23 15:28:23 +0100

    add sdkman autocomplete

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -3,8 +3,8 @@ export ZSH=$HOME/.oh-my-zsh
 # ZSH_THEME="agnoster"
 if [[ "$(uname -s)" == "Darwin" ]]
 then
-	plugins=(aws brew bundler docker gem gpg-agent git knife mvn npm osx ruby rvm sudo vagrant)
+	plugins=(aws brew bundler docker gem gpg-agent git knife mvn npm osx ruby rvm sdk sudo vagrant)
 else
-	plugins=(autoenv aws bundler docker gem git jira knife mvn npm ruby rvm safe-paste sudo vagrant)
+	plugins=(autoenv aws bundler docker gem git jira knife mvn npm ruby rvm safe-paste sdk sudo vagrant)
 fi
 source $ZSH/oh-my-zsh.sh
