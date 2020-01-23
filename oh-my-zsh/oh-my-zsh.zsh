f9ba4e02acbdd63d53dbd8ffd636724ae75f1beb
francoiskha <francois.kha@gmail.com>
2020-01-23 15:52:26 +0100

    adding some oh-my-zsh plugins
    adding some oh-my-zsh plugins

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -3,8 +3,8 @@ export ZSH=$HOME/.oh-my-zsh
 # ZSH_THEME="agnoster"
 if [[ "$(uname -s)" == "Darwin" ]]
 then
-	plugins=(aws brew bundler docker gem gpg-agent git knife mvn npm osx ruby rvm sdk sudo vagrant vault)
+	plugins=(aws brew bundler colorize docker gem git gpg-agent gradle knife mvn npm osx ruby rvm sdk sudo vagrant vault)
 else
-	plugins=(autoenv aws bundler docker gem git jira knife mvn npm ruby rvm safe-paste sdk sudo vagrant vault)
+	plugins=(autoenv aws bundler colorize docker gem git gradle jira knife mvn npm ruby rvm safe-paste sdk sudo vagrant vault ufw)
 fi
 source $ZSH/oh-my-zsh.sh
