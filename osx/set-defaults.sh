88719205dae31a08cbfa07979fe696d7a45bac5e
François Kha <fkha@oui.sncf>
2023-01-23 11:06:53 +0100

    feat: macos finder display by column

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -13,8 +13,8 @@ then
     # Disable press-and-hold for keys in favor of key repeat.
     defaults write -g ApplePressAndHoldEnabled -bool false
 
-    # Always open everything in Finder's list view. This is important.
-    defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+    # Always open everything in Finder's column view. (list == Nlsv)
+    defaults write com.apple.Finder FXPreferredViewStyle clmv
 
     # Show the ~/Library folder.
     chflags nohidden ~/Library
