e16206d08b3719b27de7391085744134a623cef3
francoiskha <francois.kha@gmail.com>
2024-11-22 23:22:53 +0100

    rip ripgrep

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -2,6 +2,6 @@
 export ZSH=$HOME/.oh-my-zsh
 if [[ "$(uname -s)" == "Darwin" ]]
 then
-	plugins=(aws brew colorize direnv docker encode64 fluxcd fzf git gpg-agent gradle helm iterm2 kubectl macos mvn nmap node npm nvm rsync ripgrep screen sdk sudo terraform vagrant vault vscode yarn)
+	plugins=(aws brew colorize direnv docker encode64 fluxcd fzf git gpg-agent gradle helm iterm2 kubectl macos mvn nmap node npm nvm rsync screen sdk sudo terraform vagrant vault vscode yarn)
 fi
 source $ZSH/oh-my-zsh.sh
