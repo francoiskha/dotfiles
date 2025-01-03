c635e3705cfff7a64928dc879cbc77ded31aa649
francoiskha <francois.kha@gmail.com>
2025-01-03 16:04:15 +0100

    fix(osx): adapt fast key repeat to Apple Silicon models

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -23,7 +23,7 @@ then
     chflags nohidden ~/Library
 
     # Set a really fast key repeat.
-    defaults write NSGlobalDomain KeyRepeat -int 0
+    defaults write NSGlobalDomain KeyRepeat -int 1
 
     # Set the Finder prefs for showing a few different volumes on the Desktop.
     defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
