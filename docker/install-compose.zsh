e3f9fc8bfa0cf89d63e5fd3e2d917b6f68076f60
francoiskha <francois.kha@gmail.com>
2026-01-24 23:32:32 +0100

    feat(docker): add echo statement for docker compose plugin installation

--- docker/install-compose.zsh
+++ docker/install-compose.zsh
@@ -2,6 +2,7 @@
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 if [ ! -d "$DOCKER_CONFIG/cli-plugins" ]; then mkdir -p $DOCKER_CONFIG/cli-plugins ; fi
 if [ ! -f "$DOCKER_CONFIG/cli-plugins/docker-compose" ]; then 
+    echo "Installing docker compose plugin..."
     curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-darwin-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
     chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 fi
